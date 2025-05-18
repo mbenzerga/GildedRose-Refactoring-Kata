92489ef8de882b56fdb1cb110bb72a088ec32a30
Mohamed Benzerga <_mohamed.benzerga.az@sodexo.com>
2025-05-18 15:19:20 +0000

    first basic tests

--- python/test_gilded_rose.py
+++ python/test_gilded_rose.py
@@ -1,16 +1,22 @@
-# -*- coding: utf-8 -*-
-import unittest
+import pytest
 
 from gilded_rose import Item, GildedRose
 
 
-class GildedRoseTest(unittest.TestCase):
-    def test_foo(self):
-        items = [Item("foo", 0, 0)]
-        gilded_rose = GildedRose(items)
-        gilded_rose.update_quality()
-        self.assertEqual("fixme", items[0].name)
+def test_item_sell_in_decreases_but_not_quality_if_sell_in_is_0():
+    items = [Item(name="foo", sell_in=0, quality=0)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "foo"
+    assert items[0].sell_in == -1
+    assert items[0].quality == 0
 
-        
-if __name__ == '__main__':
-    unittest.main()
+
+@pytest.mark.parametrize("sell_in, quality", [(1, 1), (3, 5), (8, 12)])
+def test_item_sell_in_and_quality_decrease_if_sell_in_is_positive(sell_in, quality):
+    items = [Item(name="foo", sell_in=sell_in, quality=quality)]
+    gilded_rose = GildedRose(items)
+    gilded_rose.update_quality()
+    assert items[0].name == "foo"
+    assert items[0].sell_in == sell_in - 1
+    assert items[0].quality == quality - 1
